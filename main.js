c1573763a8b53effabbd85b961c2c69209ddc13d
Gelson Carvalho <gelsonrc1@yahoo.com.br>
2023-04-14 12:01:27 -0300

    fisrt commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("form-deposito");
+
+function validaNome(nomeCompleto) {
+     const nomeComoArray = nomeCompleto.split(" ");
+     return nomeComoArray.length >= 2;
+}
+
+form.addEventListener("submit",function(e){
+     e.preventDefault(); 
+
+     const nomeBeneficiario = document.getElementById("nomeBeneficiario");
+     if (!validaNome(nomeBeneficiario.value)) {
+          alert("O Nome não está completo");
+          
+     }
+
+})
+
+console.log(form);
+
+
+
+
+
