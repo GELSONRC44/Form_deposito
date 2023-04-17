bdd527e2c271b35b469452645aece5c8f1344c92
Gelson Carvalho <gelsonrc1@yahoo.com.br>
2023-04-17 12:37:14 -0300

    first commit

--- main.js
+++ main.js
@@ -3,21 +3,34 @@ const form = document.getElementById("form-deposito");
 function validaNome(nomeCompleto) {
      const nomeComoArray = nomeCompleto.split(" ");
      return nomeComoArray.length >= 2;
+
 }
 
 form.addEventListener("submit",function(e){
+     let formValido = false;
      e.preventDefault(); 
 
      const nomeBeneficiario = document.getElementById("nomeBeneficiario");
-     if (!validaNome(nomeBeneficiario.value)) {
-          alert("O Nome não está completo");
-          
-     }
+     const numeroContaBeneficiario = document.getElementById("numero-conta");
+     const valorDeposito = document.getElementById("valor-deposito");
+     const mensagemSucesso = "Montante de": ${valorDeposito.value} "deposito para o cliente": ${nomeBeneficiario.value} - "conta":${numeroContaBeneficiario.value};
+     
+     formEValido = validanome(nomeBeneficiario.value)
+          if (formEValido) {
+               alert(mensagemSucesso)
+
+               nomeBeneficiario.value ='';
+               numeroContaBeneficiario.value ='';
+               valorDeposito.value = '';
+
+
+          }else{
+               alert("O Nome não está completo");
+             }
+       }
 
 })
-
 console.log(form);
-
 
 
 
