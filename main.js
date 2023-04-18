d05cb676fbd04b7e1b872765b0128d73fe17e3d6
Gelson Carvalho <gelsonrc1@yahoo.com.br>
2023-04-18 13:02:24 -0300

    fisrt commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById("form-deposito");
+const nomeBeneficiario = document.getElementById("nome-beneficiario");
+let formEValido = false; 
 
 function validaNome(nomeCompleto) {
      const nomeComoArray = nomeCompleto.split(" ");
@@ -7,31 +9,54 @@ function validaNome(nomeCompleto) {
 }
 
 form.addEventListener("submit",function(e){
-     let formValido = false;
+     
      e.preventDefault(); 
-
-     const nomeBeneficiario = document.getElementById("nomeBeneficiario");
+ 
+     
+ 
      const numeroContaBeneficiario = document.getElementById("numero-conta");
      const valorDeposito = document.getElementById("valor-deposito");
-     const mensagemSucesso = "Montante de": ${valorDeposito.value} "deposito para o cliente": ${nomeBeneficiario.value} - "conta":${numeroContaBeneficiario.value};
-     
-     formEValido = validanome(nomeBeneficiario.value)
-          if (formEValido) {
-               alert(mensagemSucesso)
+     const mensagemSucesso = `Montante de: <b> ${valorDeposito.value}</b> deposito para o cliente:<b>
+      ${nomeBeneficiario.value}</b> - conta:<b>${numeroContaBeneficiario.value}</b>`;
+       
+       formEValido = validaNome(nomeBeneficiario.value)
+       if (formEValido) {
+            const contaInnerMensagemSucesso = document.querySelector('.success-message');
+            contaInnerMensagemSucesso .innerHTML = mensagemSucesso;
+            contaInnerMensagemSucesso .style.display = 'block';
+          
+            nomeBeneficiario.value ='';
+            numeroContaBeneficiario.value ='';
+            valorDeposito.value = '';
 
-               nomeBeneficiario.value ='';
-               numeroContaBeneficiario.value ='';
-               valorDeposito.value = '';
 
-
-          }else{
-               alert("O Nome não está completo");
-             }
-       }
+       }else{
+          nomeBeneficiario.style.border = '1px solid red';
+          document.querySelector('.error-message').style.display = 'block';
+          }
 
 })
-console.log(form);
 
+nomeBeneficiario.addEventListener('keyup', function (e){
+     console.log(e.target.value);
+     formEValido = validaNome(e.target.value);
+
+
+     if (!formEValido) {
+          nomeBeneficiario.classList.add('error');
+           //nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    
+     }else{
+          nomeBeneficiario.style = ('.error');
+          document.querySelector('.error-message').style.display = 'none';
+     }  
+     
+    
+
+
+});
+     
 
 
 
